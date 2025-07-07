97f802f928074c80fda0dc3c32fa13e9740fc2b2
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2025-07-06 22:29:28 -0500

    feat: set route for method controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\AnimalController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('type-animal', [AnimalController::class, 'getQuantityTypeAnimal']);
